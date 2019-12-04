35c1e842a3227ffed364554f80137bbda80b01fb
windcrystal0513 <wuyunfeng1234123@163.com>
2019-12-04 14:52:32 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    
+    <router-view/>   <!-- 显示的就是当前路由地址对应的内容 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+	<home-header></home-header>
+</div>
+</template>
+
+<script>
+import HomeHeader from './component/Header.vue'
+export default{
+		name:'Home',
+		components:{
+			HomeHeader
+		}
+}
+</script>
+
+<style>
+	
+</style>
