630ab81c5150b548131e42eebaf51fcbeb943937
windcrystal0513 <wuyunfeng1234123@163.com>
2019-12-04 17:10:10 +0800

    首页轮播图

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
 <div>
 	<home-header></home-header>
+	<home-swiper></home-swiper>
 </div>
 </template>
 
 <script>
 import HomeHeader from './component/Header.vue'
+import HomeSwiper from './component/Swiper.vue'
 export default{
 		name:'Home',
 		components:{
-			HomeHeader
+			HomeHeader,
+			HomeSwiper
 		}
 }
 </script>
---
+++ src/pages/home/component/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="wrapper">
+	<swiper :options="swiperOption">
+	    <!-- slides -->
+	    <swiper-slide v-for='item of swiperList' :key='item.id'>
+	    	<img  class="swiper-img" :src="item.imgUrl" >
+	    </swiper-slide>
+	    <!-- Optional controls -->
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>	
+ </div>
+</template>
+
+<script>
+export default{
+	name:'HomeSwiper',
+	data() {
+		return{
+			swiperOption:{
+				pagination:'.swiper-pagination',
+				loop:true
+			},
+			swiperList:[{
+				id:'0001',
+				imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201912/9accc56ec000de669ce90d0b3f44af86.jpg_750x200_b35b86a0.jpg'
+			},{
+				id:'0002',
+				imgUrl:"http://img1.qunarzz.com/piao/fusion/1609/47/0cfdc871ac183702.jpg_750x200_f6896acd.jpg"
+			},{
+				id:'0003',
+				imgUrl:'http://img1.qunarzz.com/piao/fusion/1805/fe/ca8ed7adc3ff9f02.jpg_750x200_49735140.jpg'
+			}]
+		}
+	}
+}
+</script>
+
+<style lang='stylus' scoped>
+	.wrapper>>>.swiper-pagination-bullet-active
+		background:white
+	.wrapper
+		width:100%
+		height:0
+		overflow:hidden
+		padding-bottom:31.25%
+		.swiper-img
+			width:100%
+</style>
